47e0b7fddcbebe015f6710ac47501e3ba82fde86
nand09 <osortof@yahoo.com>
2015-02-28 11:40:21 -0500

    Devise, and other syntax errors

---
+++ app/views/devise/registrations/name.rb
@@ -0,0 +1,7 @@
+puts "what is your first name?"
+	firstname = gets.chomp
+puts "what is your middle name?"
+	middlename = gets.chomp
+puts "what is your last name?"
+	lastname = gets.chomp
+	puts "nice to meet you" + firstname + " " + middlename + " " + lastname + "."
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-config.action_mailer.default_url_options = { :host: => 'omr-revolutionist.com' } #note to set to actual host 
+config.action_mailer.default_url_options = { host: => 'omr-revolutionist.com' } #note to set to actual host 
 end
